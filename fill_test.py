6a0645672a159b38d75d7c7458b17c4f9f1b07c7
weibo <503483264@qq.com>
2018-09-26 09:19:27 +0800

    first commit file
    Signed-off-by: weibo <503483264@qq.com>

--- fill_test.py
+++ fill_test.py
@@ -1,10 +1,21 @@
 
+import  requests
+import json
 
-def add(a,b):
-    a=1
-    b=2
-    c=a+b
-    return c
+URL= 'https://api.github.com'
 
-if __name__=='main':
-    add(1,2)
+def build_url(endpoint):
+    return '/'.join([URL,endpoint])
+
+def better_output(json_str):
+    return json.dumps(json.loads(json_str),indent=4)
+
+def request_method():
+    response=requests.get(build_url("users/weibo532421"))
+    print(better_output(response.text))
+    print(response.url)
+
+
+
+if __name__=='__main__':
+    request_method()
